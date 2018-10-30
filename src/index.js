ba5b6dea7c1cd87c9fe8b92e0f532bc8db026d43
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2018-10-30 17:11:01 +0100

    eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-console.log("Hey, ik ben een JS file");
+console.log(`Hey, ik ben een JS file`);
 const arr = [1, 2, 3];
-const ES6Stuff = () => console.log("Ik kan ES6 stuff aan", ...arr);
+const ES6Stuff = () => console.log(`Ik kan ES6 stuff aan`, ...arr);
 ES6Stuff();
